ff45ab1bbee5bb7c3b9003a58417985b4db3e79d
Michael Szell <michael.szell@gmail.com>
2021-10-18 14:23:54 +0200

    teaching page simplification

--- teaching/index.php
+++ teaching/index.php
@@ -89,31 +89,36 @@ a.k.a. Student drop-in hours: Tue 11-12, or by appointment.
 <h4>Future semesters</h4>
 	</br>
 	<table>
-		<tr><td class='std col1 date'>2022-05-27<br />2022-01-31</td><td class='std' width='130'>Instructor (75%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=940">Geospatial Data Science (KSGEDAS1KU)</a></br>ECTS Credits: 7.5</td></tr>
+		<tr><td class='std col1 date'>Spring 2022</td><td class='std' width='130'>Instructor (75%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=940">Geospatial Data Science (KSGEDAS1KU)</a></br>ECTS Credits: 7.5</td></tr>
 	</table>
 
 <h4>Current semester</h4>
 	</br>
 	<table>
-		<tr><td class='std col1 date'>2022-01-31<br />2021-08-23</td><td class='std' width='130'>Instructor (65%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=734">Introduction to Data Science and Programming (BSINDSP1KU)</a></br>ECTS Credits: 15.0</td></tr>
+		<tr><td class='std col1 date'>Fall 2021</td><td class='std' width='130'>Instructor (65%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=734">Introduction to Data Science and Programming (BSINDSP1KU)</a></br>ECTS Credits: 15.0</td></tr>
 	</table>
 
 <h4>Past semesters</h4>
 	</br>
 	<table>
-		<tr><td class='std col1 date'>2021-06-27<br />2021-02-01</td><td class='std' width='130'>Instructor (30%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=590">First Year Project (BSFIYEP1KU)</a></br>ECTS Credits: 15.0</td></tr>
-		<tr><td class='std col1 date'>2021-01-31<br />2020-08-24</td><td class='std' width='130'>Instructor (65%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=522">Introduction to Data Science and Programming (BSINDSP1KU)</a></br>ECTS Credits: 15.0</td></tr>
+		<tr><td class='std col1 date'>Spring 2021</td><td class='std' width='130'>Instructor (30%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=590">First Year Project (BSFIYEP1KU)</a></br>ECTS Credits: 15.0</td></tr>
+		<tr><td class='std col1 date'>Fall 2020</td><td class='std' width='130'>Instructor (65%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=522">Introduction to Data Science and Programming (BSINDSP1KU)</a></br>ECTS Credits: 15.0</td></tr>
+		<tr><td class='std col1 date'>Fall 2019</td><td class='std' width='130'>Instructor (70%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=371">Technical Communication (BSTECOM1KU)</a></br>ECTS Credits: 7.5</td></tr>
+		<tr><td class='std col1 date'>Fall 2019</td><td class='std' width='130'>Instructor (50%)</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=282">Introduction to Data Science and Programming (BSINDSP1KU)</a></br>ECTS Credits: 15.0</td></tr>
+		<tr><td class='std col1 date'>Fall 2018</td><td class='std' width='130'>Instructor,</br>Course manager</td><td class='std'><a href="https://courses.ceu.edu/courses/data-and-network-visualization-0">Data and Network Visualization (DNDS 6002)</a></br>Credits: 2.0, ECTS Credits: 4.0</td></tr>
+		<tr><td class='std col1 date'>Fall 2018</td><td class='std' width='130'>Instructor,</br>Course manager</td><td class='std'><a href="https://courses.ceu.edu/courses/urban-networks-and-human-mobility">Urban Networks and Human Mobility (DNDS 6003)</a></br>Credits: 2.0, ECTS Credits: 4.0</td></tr>
+		<tr><td class='std col1 date'>Spring 2018</td><td class='std' width='130'>Instructor,</br>Course manager</td><td class='std'><a href="https://cns.ceu.edu/courses/statistical-methods-network-science-and-data-analysis">Statistical Methods in Network Science and Data Analysis (CNSC 6013)</a></br>Credits: 4.0, ECTS Credits: 8.0</td></tr>
+	</table>
+
+<h4>Single classes and short-term courses</h4>
+	</br>
+	<table>
 		<tr><td class='std col1 date'>2020-04-21</td><td class='std' width='130'>Guest instructor</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=373">Reflections on Data Science (BSREDAS1KU)</a></td></tr>
-		<tr><td class='std col1 date'>2020-01-31<br />2019-08-26</td><td class='std' width='130'>Instructor (70%),</br>Course manager</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=371">Technical Communication (BSTECOM1KU)</a></br>ECTS Credits: 7.5</td></tr>
-		<tr><td class='std col1 date'>2020-01-31<br />2019-08-26</td><td class='std' width='130'>Instructor (50%)</td><td class='std'><a href="https://learnit.itu.dk/local/coursebase/view.php?ciid=282">Introduction to Data Science and Programming (BSINDSP1KU)</a></br>ECTS Credits: 15.0</td></tr>
 		<tr><td class='std col1 date'>2019-04-17</td><td class='std' width='130'>Guest instructor</td><td class='std'>SG2 Analytisches Gestaltungsprojekt, <a href="https://www.hfg-gmuend.de/Strategische_Gestaltung_2.html">HfG Schwäbisch Gmünd</a></td></tr>
-		<tr><td class='std col1 date'>2018-12-04<br />2018-10-30</td><td class='std' width='130'>Instructor,</br>Course manager</td><td class='std'><a href="https://courses.ceu.edu/courses/data-and-network-visualization-0">Data and Network Visualization (DNDS 6002)</a></br>Credits: 2.0, ECTS Credits: 4.0</td></tr>
-		<tr><td class='std col1 date'>2018-10-25<br />2018-09-18</td><td class='std' width='130'>Instructor,</br>Course manager</td><td class='std'><a href="https://courses.ceu.edu/courses/urban-networks-and-human-mobility">Urban Networks and Human Mobility (DNDS 6003)</a></br>Credits: 2.0, ECTS Credits: 4.0</td></tr>
-		<tr><td class='std col1 date'>2018-03-29<br />2018-01-10</td><td class='std' width='130'>Instructor,</br>Course manager</td><td class='std'><a href="https://cns.ceu.edu/courses/statistical-methods-network-science-and-data-analysis">Statistical Methods in Network Science and Data Analysis (CNSC 6013)</a></br>Credits: 4.0, ECTS Credits: 8.0</td></tr>
 		<tr><td class='std col1 date'>2017-11-30</td><td class='std' width='130'>Guest instructor</td><td class='std'><a href="https://cns.ceu.edu/courses/data-and-network-visualization">Data and Network Visualization (CNSC 6012)</a></td></tr>
 		<tr><td class='std col1 date'>2017-11-24</td><td class='std' width='130'>Guest instructor</td><td class='std'><a href="https://cns.ceu.edu/courses/justdata-university-wide-course">JustData - University Wide Course</a></td></tr>
 		<tr><td class='std col1 date'>2016-08-23<br />2016-08-18</td><td class='std' width='130'>Seminar chair</td><td class='std'><a href="http://www.alpbach.org/en/session/sem16-seminar-16-understanding-big-data/">Seminar Understanding Big Data, European Forum Alpbach 2016</a><br />Alpbach, Austria</td></tr>
-	</table>
+		</table>
 
 	</div>
 
