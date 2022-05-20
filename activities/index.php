faf4d83e074a1bf7ddcf3a0e90272bc336e861fa
Michael Szell <michael.szell@gmail.com>
2022-05-20 14:56:09 +0200

    new activites

--- activities/index.php
+++ activities/index.php
@@ -28,6 +28,7 @@ echo $featuredarea;
 	<a href="#" onclick="toggle('organization');return false;" id="info-organization">&darr; Toggle all activities &darr;</a>
 	<div class="expansion" id="organization">
 	<table>
+		<tr><td class='std col1 date'>2022-11-04</td><td class='std' width='130'>Program committee</td><td class='std'><a href="https://www.cityvis.io/workshops/2022/">CityVis 2022</a><br />Potsdam, Germany</td></tr>
 		<tr><td class='std col1 date'>2022-06-09<br />2022-06-06</td><td class='std' width='130'>Co-chair dataset track</td><td class='std'><a href="https://icwsm.org/2022/index.html/">ICWSM-22</a><br />Atlanta, USA</td></tr>
 		<tr><td class='std col1 date'>2021-12-02<br />2021-11-30</td><td class='std' width='130'>Program committee</td><td class='std'><a href="http://www.complexnetworks.org/">Complex Networks 2021</a><br />Madrid, Spain</td></tr>
 		<tr><td class='std col1 date'>2021-10-28<br />2021-10-27</td><td class='std' width='130'>Program committee</td><td class='std'><a href="https://sites.google.com/view/mimode2021/">The 2nd workshop on Migration and Mobility Research in the Digital Era (MIMODE 2021)</a><br />Lyon, France</td></tr>
@@ -95,6 +96,11 @@ echo $featuredarea;
 
 <h3>Talks</h3>
 	<ul>
+	<li class='talk talk-invited'>
+	<table>
+	<tr><td class='std col1 date'>2022-05-24</td><td class='std'>Cycling research at NERDS<br />ISI workshop<br/>Turin, Italy</td></tr>
+	</table>
+	</li>
 	<li class='talk talk-invited'>
 	<table>
 	<tr><td class='std col1 date'>2022-03-11<br />2022-03-09<br /><a href="../downloads/recording_szell2022thu.mp4"><div class="linkexternal">Recording</div></a><br /><a href="../downloads/talk_szell2022thu.pdf"><div class="filepdf">Slides</div></a></td><td class='std'>Towards a human-centric urban data science<br /><a href="https://www.pks.mpg.de/phymo22">Physics and Collective Dynamics of Future Mobility</a><br/>Dresden, Germany (remotely)</td></tr>
