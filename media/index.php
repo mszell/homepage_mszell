26a16aa9ede13d6289dd3121cd36dc7651c0b09c
Michael Szell <michael.szell@gmail.com>
2022-06-06 11:40:27 +0200

    new media/radio coverage

--- media/index.php
+++ media/index.php
@@ -32,6 +32,7 @@ echo $featuredarea;
 <h3>Media coverage (selection)</h3>
 	<table>
 	    <!--<tr><td class='std col1 date'>2010-07</td><td class='std'>XXXXX</td><td class='std'>YYYYYY<br /><a href="ZZZZZZ"><div class="linkexternal">Online report</div></a></td></tr> -->
+	    <tr><td class='std col1 date'>2022-06-02</td><td class='std'>Radio Ö1</td><td class='std'>Dimensionen - Planung funktionaler städtischer Radnetze<br /><a href="https://oe1.orf.at/player/20220602/681583/1654189908281"><div class="linkexternal">Radio show</div></a> &nbsp; <a href="../downloads/radio_oe101.mp3"><div class="linkexternal">Recording</div></a></td></tr>
 	    <tr><td class='std col1 date'>2022-05-24</td><td class='std'>Kleine Zeitung</td><td class='std'>Stückwerk ist die schlechteste Strategie<br /><a href="https://www.kleinezeitung.at/auto/6134526/Radwegnetze-fuer-Staedte_Stueckwerk-ist-die-schlechteste-Strategie"><div class="linkexternal">Online report</div></a></td></tr>
 	    <tr><td class='std col1 date'>2022-05-13</td><td class='std'>futurezone</td><td class='std'>Wie man mehr Menschen zum Umstieg auf das Fahrrad bewegt<br /><a href="https://futurezone.at/science/menschen-umstieg-fahrrad-radfahren-radverkehr-netz-radwege-infrastruktur/402005559"><div class="linkexternal">Online report</div></a></td></tr>
 	    <tr><td class='std col1 date'>2022-05-04</td><td class='std'>Science ORF.at</td><td class='std'>Das optimale Radwegnetz<br /><a href="https://science.orf.at/stories/3212926/"><div class="linkexternal">Online report</div></a></td></tr>
