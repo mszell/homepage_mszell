b0fd83df2062fad64b23e1a9eb254637f2578d95
Michael Szell <michael.szell@gmail.com>
2022-11-06 15:14:44 +0100

    mastodon widget improvements

--- constants.php
+++ constants.php
@@ -161,7 +161,7 @@ EOD;
 
 $footer = <<<EOD
 <div id="footer">
-  <p>{$URLTOP} &nbsp;|&nbsp; Last update 2022-11-05 &nbsp;|&nbsp; <a href="https://github.com/mszell/homepage_mszell">Source code</a></p>
+  <p>{$URLTOP} &nbsp;|&nbsp; Last update 2022-11-06 &nbsp;|&nbsp; <a href="https://github.com/mszell/homepage_mszell">Source code</a></p>
 </div>
 EOD;
 
