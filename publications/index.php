4389404eca89eb84cbc5c1c768c7b1cfdf9aba75
Michael Szell <michael.szell@gmail.com>
2023-08-07 14:39:44 +0200

    Add toggles

--- publications/index.php
+++ publications/index.php
@@ -25,12 +25,16 @@ echo $featuredarea;
 
 <h3>My coauthorship network</h3>
 <div id="collabnet"><script src="http://michael.szell.net/publications/collabnet.js" type="text/javascript"></script></div>
-<p>Last update 2022-11-03</br>
-Node color: Most recent date of publication with me</br>
-Node size: Duration of collaboration with me</br>
-Link weight: Number of co-publications<br/>
-Link color: Co-publication with me (red) / between my co-authors (grey)</p>
-<br/>
+
+</br></br></br>
+<a href="#" onclick="toggle('legend');return false;" id="info-legend">&darr; Toggle legend &darr;</a>
+<div class="expansion" id="legend">
+  <p>Last update 2022-11-03</br>
+  Node color: Most recent date of publication with me</br>
+  Node size: Duration of collaboration with me</br>
+  Link weight: Number of co-publications<br/>
+  Link color: Co-publication with me (red) / between my co-authors (grey)</p><br/>
+</div>
 
 <p>
 Download bib file of my publications: <a href="../downloads/szell.bib">szell.bib</a></br>
@@ -41,12 +45,16 @@ Download a list of my publications: <a href="../downloads/publicationlist_szell.
 <ul>
   <li><b><a href="../downloads/article_politiken01.pdf"><div class="filepdf">Er Danmark en cykelnation?</div></a></b>, Politiken (2022)</li>
   <li><b><a href="https://blogs.biomedcentral.com/on-physicalsciences/2022/05/06/identifying-urban-features-for-vulnerable-road-user-safety-in-europe/">Identifying urban features for vulnerable road user safety in Europe</a></b>, BMC Blog Network (2022)</li>
-  <li><b><a href="https://www.altinget.dk/transport/artikel/14-forskere-videnscenter-for-cyklisme-skal-vaere-uafhaengigt-og-forskningsbaseret-ikke-hoere-under-vejdirektoratet">14 forskere: Videnscenter for cyklisme skal ikke høre under Vejdirektoratet</a></b>, Altinget (2021)</li>
-  <li><b><a href="https://pro.ing.dk/mobilitytech/holdning/baeredygtige-byer-kraever-maalrettet-investering-i-cykelinfrastruktur">Bæredygtige byer kræver målrettet investering i cykelinfrastruktur</a></b>, Ingeniøren / Mobilitytech (2021)</li>
-  <li><b><a href="https://finans.dk/debat/ECE13173026/brug-ny-mobilitetslov-til-at-skabe-et-mere-baeredygtigt-byliv/">Brug ny mobilitetslov til at skabe et mere bæredygtigt byliv</a></b>, FINANS (2021)</li>
-  <li><b><a href="https://pro.ing.dk/mobilitytech/holdning/cykling-i-byen-smaa-investeringer-i-infrastruktur-binder-din-sammen">Cykling i byen: Små investeringer i infrastruktur binder din by sammen</a></b>, Ingeniøren / Mobilitytech (2021)</li>
 </ul>
-
+<a href="#" onclick="toggle('nonacademic');return false;" id="info-nonacademic">&darr; Toggle all publications &darr;</a>
+<div class="expansion" id="nonacademic">
+  <ul>
+    <li><b><a href="https://www.altinget.dk/transport/artikel/14-forskere-videnscenter-for-cyklisme-skal-vaere-uafhaengigt-og-forskningsbaseret-ikke-hoere-under-vejdirektoratet">14 forskere: Videnscenter for cyklisme skal ikke høre under Vejdirektoratet</a></b>, Altinget (2021)</li>
+    <li><b><a href="https://pro.ing.dk/mobilitytech/holdning/baeredygtige-byer-kraever-maalrettet-investering-i-cykelinfrastruktur">Bæredygtige byer kræver målrettet investering i cykelinfrastruktur</a></b>, Ingeniøren / Mobilitytech (2021)</li>
+    <li><b><a href="https://finans.dk/debat/ECE13173026/brug-ny-mobilitetslov-til-at-skabe-et-mere-baeredygtigt-byliv/">Brug ny mobilitetslov til at skabe et mere bæredygtigt byliv</a></b>, FINANS (2021)</li>
+    <li><b><a href="https://pro.ing.dk/mobilitytech/holdning/cykling-i-byen-smaa-investeringer-i-infrastruktur-binder-din-sammen">Cykling i byen: Små investeringer i infrastruktur binder din by sammen</a></b>, Ingeniøren / Mobilitytech (2021)</li>
+  </ul>
+</div>
 
 <h3>2023</h3>
   <ul>
