4cd6d2eed11cb03c7969498caf09543e77b72109
Michael Szell <michael.szell@gmail.com>
2024-01-27 11:56:11 +0100

    Add JUST STREETS news

--- constants.php
+++ constants.php
@@ -160,7 +160,7 @@ EOD;
 
 $footer = <<<EOD
 <div id="footer">
-  <p>{$URLTOP} &nbsp;|&nbsp; Last update 2023-12-15 &nbsp;|&nbsp; <a href="https://github.com/mszell/homepage_mszell">Source code</a></p>
+  <p>{$URLTOP} &nbsp;|&nbsp; Last update 2024-01-27 &nbsp;|&nbsp; <a href="https://github.com/mszell/homepage_mszell">Source code</a></p>
 </div>
 EOD;
 
