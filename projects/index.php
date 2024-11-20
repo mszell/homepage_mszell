e2882f5a348b35ca278caec35f3d746b647830eb
Michael Szell <michael.szell@gmail.com>
2024-11-20 10:28:17 +0100

    Add funded grant projects structure to projects

--- projects/index.php
+++ projects/index.php
@@ -16,14 +16,20 @@ echo $featuredarea;
 
 			<div class="main-with-sidebar">
 
-<h3>superblockify</h3>
+<h3>Funded grant projects</h3>
+tba
+
+<p>For further funded projects where I was not PI, see my <a href="../downloads/cv_szell.pdf">CV</a>.</p>
+
+<h3>Data visualization and software projects</h3>
+<h4>superblockify</h4>
     <table>
 		<tr><td class='std col1 date'>2024-04</td><td class='std' style='text-align: center;'><a href="https://superblockify.city"><img src="../images/logosuperblockify.png" width="58" height="58" alt="superblockify" class="logo"></a></td><td class='std'><p>A Python package for partitioning an urban street network into Superblock-like neighborhoods and for visualizing and analyzing the partition results. A Superblock is a set of adjacent urban blocks where vehicular through traffic is prevented or pacified, giving priority to people walking and cycling.</p>
 			<a href="https://superblockify.city/"><div class='linkexternal'>superblockify.city</div></a>
 		</td></tr>
 	</table>
 
-<h3>datasci.social</h3>
+<h4>datasci.social</h4>
     <table>
 		<tr><td class='std col1 date'>2022-11</td><td class='std' style='text-align: center;'><a href="https://datasci.social"><img src="../images/logodatascisocial.png" width="58" height="58" alt="GrowBike.Net" class="logo"></a></td><td class='std'><p>A Mastodon community of nice & diverse people researching human-centric data science and adjacent topics, like human-centric data/network science, social data science, computational social science.</p>
 			<a href="https://datasci.social"><div class='linkexternal'>datasci.social</div></a>
@@ -31,7 +37,7 @@ echo $featuredarea;
 	</table>
 
 
-<h3>GrowBike.Net</h3>
+<h4>GrowBike.Net</h4>
     <table>
 		<tr><td class='std col1 date'>2021-07</td><td class='std' style='text-align: center;'><a href="https://growbike.net"><img src="../images/logogrowbikenet.png" width="58" height="58" alt="GrowBike.Net" class="logo"></a></td><td class='std'><p>An interactive visualization platform growing cohesive urban bicycle networks - something that every modern city should have. Studying these synthetic networks informs us about the geometric limitations of urban bicycle network growth and can lead to better designed bicycle infrastructure in cities.</p>
 		<p>GrowBike.Net accompanies the scientific paper "<a href="https://www.nature.com/articles/s41598-022-10783-y">Growing Urban Bicycle Networks</a>".</p>
@@ -39,7 +45,7 @@ echo $featuredarea;
 		</td></tr>
 	</table>
 
-<h3>Taxonomy of Urban Bicycle Network Approaches</h3>
+<h4>Taxonomy of Urban Bicycle Network Approaches</h4>
     <table>
 		<tr><td class='std col1 date'>2021-01</td><td class='std' style='text-align: center;'><a href="https://github.com/mszell/taxonomybikenw"><img src="../images/logobikenwapproaches.png" width="58" height="58" alt="Taxonomy of Urban Bicycle Network Approaches" class="logo"></a></td><td class='std'><p>This visualization identifies the most distinct environments for cycling arriving from different approaches of urban planning.</p>
 			<a href="https://github.com/mszell/taxonomybikenw"><div class='linkexternal'>Taxonomy of Urban Bicycle Network Approaches</div></a>
@@ -47,14 +53,14 @@ echo $featuredarea;
 	</table>	
 
 
-<h3>Stephen Hawking Career</h3>
+<h4>Stephen Hawking Career</h4>
     <table>
 		<tr><td class='std col1 date'>2018-03</td><td class='std' style='text-align: center;'><a href="http://michael.szell.net/hawking/"><img src="../images/logohawking.png" width="58" height="58" alt="Stephen Hawking Career" class="logo"></a></td><td class='std'><p>A Science of Science tribute to Stephen Hawking. This interactive plot allows the exploration of the influential physicist's oeuvre - scientific papers and books - and its impact.</p>
 			<a href="http://michael.szell.net/hawking/"><div class='linkexternal'>Stephen Hawking - An interactive history of his career</div></a>
 		</td></tr>
 	</table>	
 
-<h3>What the Street!?</h3>
+<h4>What the Street!?</h4>
     <table>
 		<tr><td class='std col1 date'>2017-06</td><td class='std' style='text-align: center;'><a href="https://whatthestreet.com/"><img src="../images/logowhatthestreet.png" width="58" height="58" alt="What the Street!?" class="logo"></a></td><td class='std'><p>An online platform for the interactive exploration of city-wide mobility spaces. The aim of What the Street!? is to facilitate the intuitive exploration of (wasted) mobility space in cities, exploring to which extent space is distributed unevenly between different modes of transportation, and how massive areas of car parking could be regained for the public. What the Street?! was added to <a href="https://futurium.de/">Futurium</a>'s permanent exhibition in 2019.</a></p>
 			<a href="https://whatthestreet.com/"><div class='linkexternal'>What the Street!?</div></a> &nbsp; <a href="http://michael.szell.net/whatthestreet/"><div class='linkexternal'>Posters</div></a>
@@ -70,7 +76,7 @@ echo $featuredarea;
 		</td></tr>
 	</table>				
 
-<h3>HubCab</h3>
+<h4>HubCab</h4>
     <table>
 		<tr><td class='std col1 date'>2014-03</td><td class='std' style='text-align: center;'><a href="http://hubcab.org/"><img src="../images/logohubcab.png" width="58" height="58" alt="Hubcab" class="logo"></a></td><td class='std'><p>An online tool for visualizing and exploring taxi traces and taxi shareability in New York City, related to the scientific paper "<a href="http://www.pnas.org/content/111/37/13290">Quantifying the benefits of vehicle pooling with shareability networks</a>".</p>
 			<a href="http://hubcab.org/"><div class='linkexternal'>HubCab</div></a>
@@ -87,7 +93,7 @@ echo $featuredarea;
 		</td></tr>
 	</table>
 
-<h3>Tweet Bursts</h3>
+<h4>Tweet Bursts</h4>
 	<table>
 		<tr><td class='std col1 date'>2014-02</td><td class='std' style='text-align: center;'><a href="http://senseable.mit.edu/tweetbursts/"><img src="../images/logotweetbursts.png" width="58" height="58" alt="Tweet Bursts" class="logo"></a></td><td class='std'><p>Webpage and visualizations accompanying the scientific paper "<a href="http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0089052">Contraction of online response to major events</a>".</p>
 			<a href="http://senseable.mit.edu/tweetbursts/"><div class='linkexternal'>Tweet Bursts</div></a>
@@ -97,7 +103,7 @@ echo $featuredarea;
 		</td></tr>
 	</table>
 
-<h3>Pardus</h3>
+<h4>Pardus</h4>
     <table>
 		<tr><td class='std col1 date'>since<br />2004-07</td><td class='std' style='text-align: center;'><a href="http://www.pardus.at/"><img src="../images/logopardus.png" width="58" height="58" alt="Pardus" class="logo"></a></td><td class='std'><p><a href="http://en.wikipedia.org/wiki/Pardus_game">Wikipedia</a>: Pardus is an award-winning graphical browser-based MMORPG written in PHP and C/C++ and set in a futuristic universe where players interact and compete in space. It is a persistent-universe, open-ended game with a player-driven economy. Players travel through hundreds of "sectors" or solar systems while trading, building or battling with Non-Player Characters (NPCs) and other players. There are dozens of spacecraft models in Pardus, which can be customized with different weapons, armor and other equipment. Pardus characters do not have a defined 'class'.</p>
 		<p>Partially inspired by classic computer games such as Elite and Master of Orion II, Pardus was developed and published by the Austrian company Bayer &amp; Szell OG. Alpha testing of the game began September 2004 and the core feature set was finished by October 1, 2006.</p>
@@ -107,7 +113,7 @@ echo $featuredarea;
 
 
 
-<h3>Oil Imperium</h3>
+<h4>Oil Imperium</h4>
 	<table>
 		<tr><td class='std col1 date'>2000</td><td class='std' style='text-align: center;'><a href="http://www.ticalc.org/archives/files/fileinfo/346/34680.html"><img src="../images/projects_oilimperium.png" width="64" height="64" alt="Oil Imperium" class="logo"></a></td><td class='std'><p>Based on reLINE's 1989 PC game "OIL IMPERIUM", you are a manager trying to make money with oil. There exist 4 world regions with separate price indices, where you are able to purchase fields and sell your extracted oil. As the game takes its course, fires break out, sabotages can happen, and delivery treaties may be signed. The TI-92 game was <a href="http://www.ticalc.org/archives/news/articles/11/114/114076.html">featured on ticalc.org</a>.</p>
 			<a href="http://www.ticalc.org/archives/files/fileinfo/346/34680.html"><div class='linkexternal'>Ticalc</div></a></td></tr>
